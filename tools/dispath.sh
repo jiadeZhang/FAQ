318c9365451ddf1b6f110323f63c5465366f79e4
polostart <87565991@qq.com>
2016-03-04 22:55:38 +0800

    Create dispath.sh
    linux files rsync

---
+++ tools/dispath.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+FILES=(server_trunk/bin/GateServerD server_trunk/bin/DBServerD server_trunk/sharedata)
+TARGETPATH=/tmp/server
+DEST=(115.159.117.118)
+USER=root
+PASSWORD=topform@2107
+EXCEPT=.svn
+
+RSYNC_FLAG=rzvI
+
+for IP in ${DEST[@]}
+do
+	echo -e "$IP:$TARGETPATH\n"
+	for i in ${FILES[@]}
+	do 
+		if [ -d $i ]; then
+			echo -e "\n\033[34m******sync $i -->>  $IP$TARGETPATH/$i\033[0m"
+			ssh $USER@$IP mkdir -p $TARGETPATH/$i 
+			rsync -$RSYNC_FLAG --exclude "$EXCEPT" $i $USER@$IP:$TARGETPATH/$i
+		else
+			echo -e  "\n\033[34m******sync $i  -->>  $DEST$TARGETPATH/$i\033[0m"
+			DIR=$TARGETPATH/$i
+			ssh $USER@$IP mkdir -p ${DIR%/*}
+ 			rsync -$RSYNC_FLAG $i $USER@$IP:$TARGETPATH/$i
+		fi
+	done
+done
+
+echo -e "complete!\n";
+
+
