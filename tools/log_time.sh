5089c3fa18a0f713dd4315d038680a49f0810703
polostart <87565991@qq.com>
2016-12-14 20:30:58 +0800

    log time

---
+++ tools/log_time.sh
@@ -0,0 +1,3 @@
+﻿ps -ef | grep "cmd_id" *.log > cmd.log
+awk -F "[=,]" 'BEGIN {count=0;} {name[count]=$2;esp[count]=$6 count++;}; NR>=2{print $2,$4,$6}' cmd.log > rst.log
+awk -F ' ' 'BEGIN{count=0;} {a[$1]+=$3; t[$1]+=$2} END{for(i in a) printf"%d\t%d\t%f\t%f\t\n", i, t[i], a[i],(a[i]/t[i])}' rst.log>total.log
